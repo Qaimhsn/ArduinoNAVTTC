b9beb63abd3e792f428fc66dbf8eed41a0b7b116
Qaimhsn <qaimhasan16@gmail.com>
2020-12-03 17:51:16 +0500

    BUTTON-ON/OFF

---
+++ qaimarduino.ino
@@ -0,0 +1,24 @@
+int inp;
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);  
+}
+
+void loop() {
+if (Serial.available()>0)
+{
+  inp=Serial.read();
+  if (inp==1)
+  {
+    digitalWrite(13,HIGH);
+    (10000);
+  }
+   else if (inp==3)
+  {
+    digitalWrite(13,LOW);
+  }
+}
+
+
+
+}
