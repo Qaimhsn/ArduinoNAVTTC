ae575e8fd3e87ad572112a8c073e55fbfc05b202
Qaimhsn <qaimhasan16@gmail.com>
2020-12-09 16:22:36 +0500

    Add files via upload

---
+++ sensoradvnce.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(12,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensval =analogRead(A0);
+  int sensval1 =analogRead(A1);
+  
+  float volt=sensval*(5/1023.0);
+  float volt1=sensval1*(5/1023.0);
+  Serial.println(volt);
+  Serial.println(volt1);
+  if(volt >0.51 && volt1 >0.51)
+  {
+    digitalWrite(12,HIGH);
+  }
+  else
+  {
+    digitalWrite(12,LOW);
+  }
+}
---
+++ sensorbasic.ino
@@ -0,0 +1,14 @@
+float x;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  x=analogRead(A0);
+  float senval=x*(5.0/1023.0);
+  Serial.println(senval);
+}
+
+
+
+
