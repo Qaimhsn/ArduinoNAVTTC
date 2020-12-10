d12fbe2a7e73671e659e9495123289158b82ff05
Qaimhsn <qaimhasan16@gmail.com>
2020-12-10 19:29:17 +0500

    Add files via upload

---
+++ analogwrite-proteus.ino
@@ -0,0 +1,21 @@
+int pwm= 11;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pwm,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for (int i=0; i<=255; i++){
+  analogWrite (pwm,i);
+  Serial.println(i);
+  delay(0.5);
+}
+for (int j=255; j>=0; j--)
+{
+  analogWrite(pwm,j);
+  Serial.println(j);
+  delay(0.5);
+}
+}
